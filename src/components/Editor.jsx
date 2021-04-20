46e13a70c3ca744a6697852b109dfb3ef042d634
Stephan Arenswald <stephan@sarensw.com>
2021-04-20 14:12:19 +0200

    added basic editor and date block

---
+++ src/components/Editor.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { CompositeDecorator, Editor as DraftJsEditor, EditorState } from 'draft-js'
+import BlockDate, { BlockDateSettings } from './blocks/BlockDate'
+
+const Editor = _ => {
+  const compositeDecorator = new CompositeDecorator([
+    BlockDateSettings.decorator
+  ])
+
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty(compositeDecorator))
+
+  /**
+   * Called when the editor state changes
+   * @param {EditorState} editorState The editor state that changed
+   */
+  const onChange = editorState => {
+    console.log(editorState.getCurrentContent())
+    console.log(editorState.toJS())
+    setEditorState(editorState)
+  }
+
+  return (
+    <>
+      <div className="bg-gray-100">
+        <DraftJsEditor editorState={editorState} onChange={onChange} />
+      </div>
+    </>
+  )
+}
+
+export default Editor
