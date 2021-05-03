ba9b5deacbd3b3c0b083db2f71efc53e2396fbce
Stephan Arenswald <stephan@sarensw.com>
2021-05-03 16:43:30 +0200

    fixed block date

--- src/components/blocks/BlockDate.jsx
+++ src/components/blocks/BlockDate.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import BlockHelper from '../../helper/blockHelper'
-import df from 'dateformat'
-import DatePicker from 'react-datepicker'
 
 const BlockDate = (props) => {
-  console.log(props)
-  const date = df(new Date(), 'yy-mm-dd')
   return (
     <span style={{
       background: '#a3be8c'
     }}
       className=''
       data-offset-key={props.offsetKey}>
-      {props.children}{date}
+      {props.children}
     </span>
   )
 }
 
-const blockDateRegex = /\/\//g
+const blockDateRegex = /\/\/\d\d-\d\d-\d\d/g
 
 const blockDateStrategy = (contentBlock, callback, contentState) => {
   BlockHelper.findWithRegex(blockDateRegex, contentBlock, callback)
