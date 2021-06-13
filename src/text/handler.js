3099344dd54e7371e1d0dc6415f3591edb9f517f
Stephan Arenswald <stephan@sarensw.com>
2021-06-13 11:52:34 +0200

    added first try of a task handler

--- src/text/handler.js
+++ src/text/handler.js
@@ -3,7 +3,7 @@ import taskHandler from './handlerTask'
 const handleTextInput = (editor, data) => {
   // task handler
   taskHandler.handleNewTask(editor, data)
-  taskHandler.handleToggleTask(editor, data)
+  // taskHandler.handleToggleTask(editor, data)
 }
 
 export default {
