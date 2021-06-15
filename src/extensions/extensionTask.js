ceed6a6fb05a2ec8e3e3fb33a9b3a883384c7942
Stephan Arenswald <stephan@sarensw.com>
2021-06-15 08:50:57 +0200

    added new task handling

---
+++ src/extensions/extensionTask.js
@@ -0,0 +1,105 @@
+import { EditorView } from '@codemirror/view'
+
+/**
+ * Handles the creation of a new task
+ * @param {EditorView} view the editor view
+ * @param {number} from starting point position
+ * @param {number} to ending point position
+ * @param {string} insert character inserted
+ * @returns true in case transaction returned, false otherwise
+ */
+function handleNewTask (view, from, to, insert) {
+  console.log({ view, from, to, insert })
+  if (insert === ']') {
+    console.log('] found')
+    if (view.state.doc.sliceString(from - 1, to) === '[') {
+      const tr = view.state.update({
+        changes: [
+          { from, insert: ' ] ' }
+        ],
+        selection: { anchor: from + 3 },
+        scrollIntoView: true
+      })
+      view.dispatch(tr)
+      return true
+    }
+    return false
+  } else {
+    return false
+  }
+}
+
+function prevChar (view, from, length) {
+  return view.state.doc.sliceString(from - length, from)
+}
+
+function nextChar (view, from, length) {
+  return view.state.doc.sliceString(from, from + length)
+}
+
+/**
+ * Handles the toggling a task from done to not done and vv
+ * @param {EditorView} view the editor view
+ * @param {number} from starting point position
+ * @param {number} to ending point position
+ * @param {string} insert character inserted
+ * @returns true in case transaction returned, false otherwise
+ */
+function handleToggleTask (view, from, to, insert) {
+  if (insert === 'x') {
+    // handle x (marking task as done)
+    if (prevChar(view, from, 1) === '[' && nextChar(view, from, 2) === ' ]') {
+      const tr = view.state.update({
+        changes: [
+          { from, to: to + 2, insert: 'x]' }
+        ],
+        scrollIntoView: true
+      })
+      view.dispatch(tr)
+      return true
+    }
+    if (prevChar(view, from, 2) === '[ ' && nextChar(view, from, 1) === ']') {
+      const tr = view.state.update({
+        changes: [
+          { from: from - 1, to: to + 1, insert: 'x]' }
+        ],
+        selection: { anchor: from },
+        scrollIntoView: true
+      })
+      view.dispatch(tr)
+      return true
+    }
+
+    // handle space (marking task as not done)
+    if (prevChar(view, from, 1) === '[' && nextChar(view, from, 2) === 'x]') {
+      const tr = view.state.update({
+        changes: [
+          { from, to: to + 2, insert: ' ]' }
+        ],
+        scrollIntoView: true
+      })
+      view.dispatch(tr)
+      return true
+    }
+    if (prevChar(view, from, 2) === '[x' && nextChar(view, from, 1) === ']') {
+      const tr = view.state.update({
+        changes: [
+          { from: from - 1, to: to + 1, insert: ' ]' }
+        ],
+        selection: { anchor: from },
+        scrollIntoView: true
+      })
+      view.dispatch(tr)
+      return true
+    }
+  }
+
+  return false
+}
+
+export function newTask () {
+  return [
+    EditorView.inputHandler.of(handleNewTask),
+    EditorView.inputHandler.of(handleToggleTask)
+  ]
+}
---
+++ src/extensions/extensions.js
@@ -0,0 +1,72 @@
+import { defaultKeymap } from '@codemirror/commands'
+import { commentKeymap } from '@codemirror/comment'
+import { foldGutter, foldKeymap } from '@codemirror/fold'
+import { highlightActiveLineGutter, lineNumbers } from '@codemirror/gutter'
+import { defaultHighlightStyle } from '@codemirror/highlight'
+import { history, historyKeymap } from '@codemirror/history'
+import { indentOnInput } from '@codemirror/language'
+import { lintKeymap } from '@codemirror/lint'
+import { bracketMatching } from '@codemirror/matchbrackets'
+import { highlightSelectionMatches, searchKeymap } from '@codemirror/search'
+import { EditorState } from '@codemirror/state'
+import { drawSelection, highlightActiveLine, highlightSpecialChars, keymap } from '@codemirror/view'
+
+/// This is an extension value that just pulls together a whole lot of
+/// extensions that you might want in a basic editor. It is meant as a
+/// convenient helper to quickly set up CodeMirror without installing
+/// and importing a lot of packages.
+///
+/// Specifically, it includes...
+///
+///  - [the default command bindings](#commands.defaultKeymap)
+///  - [line numbers](#gutter.lineNumbers)
+///  - [special character highlighting](#view.highlightSpecialChars)
+///  - [the undo history](#history.history)
+///  - [a fold gutter](#fold.foldGutter)
+///  - [custom selection drawing](#view.drawSelection)
+///  - [multiple selections](#state.EditorState^allowMultipleSelections)
+///  - [reindentation on input](#language.indentOnInput)
+///  - [the default highlight style](#highlight.defaultHighlightStyle) (as fallback)
+///  - [bracket matching](#matchbrackets.bracketMatching)
+///  - [bracket closing](#closebrackets.closeBrackets)
+///  - [autocompletion](#autocomplete.autocompletion)
+///  - [rectangular selection](#rectangular-selection.rectangularSelection)
+///  - [active line highlighting](#view.highlightActiveLine)
+///  - [active line gutter highlighting](#gutter.highlightActiveLineGutter)
+///  - [selection match highlighting](#search.highlightSelectionMatches)
+///  - [search](#search.searchKeymap)
+///  - [commenting](#comment.commentKeymap)
+///  - [linting](#lint.lintKeymap)
+///
+/// (You'll probably want to add some language package to your setup
+/// too.)
+///
+/// This package does not allow customization. The idea is that, once
+/// you decide you want to configure your editor more precisely, you
+/// take this package's source (which is just a bunch of imports and
+/// an array literal), copy it into your own code, and adjust it as
+/// desired.
+export const braindumpExtensions = [
+  lineNumbers(),
+  highlightActiveLineGutter(),
+  /* highlightSpecialChars(), */
+  history(),
+  foldGutter(),
+  drawSelection(),
+  indentOnInput(),
+  defaultHighlightStyle.fallback,
+  bracketMatching(),
+  highlightActiveLine(),
+  highlightSelectionMatches(),
+  keymap.of([
+    ...defaultKeymap,
+    ...searchKeymap,
+    ...historyKeymap,
+    ...foldKeymap,
+    ...commentKeymap,
+    ...lintKeymap
+  ])
+]
+
+export { EditorView } from '@codemirror/view'
+export { EditorState } from '@codemirror/state'
