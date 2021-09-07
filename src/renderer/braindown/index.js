388414619a24198a3bd1c74c691f0ad0a06bff39
Stephan Arenswald <stephan@sarensw.com>
2021-09-07 06:20:04 +0200

    removed some commented out code

--- src/renderer/braindown/index.js
+++ src/renderer/braindown/index.js
@@ -29,47 +29,6 @@ const registerBraindownLanguage = monaco => {
       }
     })
 
-    // Define a new theme that contains only rules that match this language
-    /* monaco.editor.defineTheme('braindownTheme', {
-      base: 'vs',
-      inherit: false,
-      rules: [
-        { token: 'custom-info', foreground: '808080' },
-        { token: 'custom-error', foreground: 'ff0000', fontStyle: 'bold' },
-        { token: 'custom-notice', foreground: 'FFA500' },
-        { token: 'custom-date', foreground: '008800' },
-      ]
-    }); */
-
-    // Register a completion item provider for the new language
-    monaco.languages.registerCompletionItemProvider('braindown', {
-      provideCompletionItems: () => {
-        const suggestions = [{
-          label: 'simpleText',
-          kind: monaco.languages.CompletionItemKind.Text,
-          insertText: 'simpleText'
-        }, {
-          label: 'testing',
-          kind: monaco.languages.CompletionItemKind.Keyword,
-          insertText: 'testing(${1:condition})',
-          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
-        }, {
-          label: 'ifelse',
-          kind: monaco.languages.CompletionItemKind.Snippet,
-          insertText: [
-            'if (${1:condition}) {',
-            '\t$0',
-            '} else {',
-            '\t',
-            '}'
-          ].join('\n'),
-          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-          documentation: 'If-Else Statement'
-        }]
-        return { suggestions: suggestions }
-      }
-    })
-
     monaco.languages.registerOnTypeFormattingEditProvider('braindown', {
       autoFormatTriggerCharacters: [']'],
       /** @param {import('monaco-editor/esm/vs/editor/editor.api').editor.IModel} model */
@@ -197,15 +156,6 @@ const registerBraindownLanguage = monaco => {
         return ranges
       }
     })
-
-    // monaco.editor.create(document.getElementById("container"), {
-    //   theme: 'braindownTheme',
-    //   value: getCode(),
-    //   language: 'braindown'
-    // });
-    // monaco.editor.setTheme('braindownTheme')
-    // monaco.editor.setModelLanguage(monaco.editor.getModels()[0], 'braindown')
-    // monaco.editor.setLanguage('braindown')
   }
 }
 
