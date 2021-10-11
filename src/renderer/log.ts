77a893782694c17f3bc9d8ac46878c943130451b
Stephan Arenswald <stephan@sarensw.com>
2021-10-11 04:45:53 +0200

    chore: changed log.js to ts

--- src/renderer/log.ts
+++ src/renderer/log.ts
@@ -1,6 +1,6 @@
 import stringify from 'json-stringify-safe'
 
-const logToMain = (type, msg) => {
+const logToMain = (type: string, msg: string): void => {
   const message = (typeof msg === 'object' && msg !== null ? stringify(msg, null, 2) : msg)
   window.__preload.send({
     channel: 'log',
@@ -11,24 +11,25 @@ const logToMain = (type, msg) => {
   })
 }
 
-const debug = (msg, ...args) => {
+const debug = (msg: string, ...args: string[]): void => {
   let tm = ''
   for (const arg of args) {
     tm += `[${arg}]`
   }
-  tm += tm.length === 0 ? stringify(msg, null, 2) : ` ${stringify(msg, null, 2)}`
+  const s = stringify(String(msg), null, 2)
+  tm += tm.length === 0 ? String(s) : ` ${String(s)}`
   logToMain('debug', tm)
 }
 
-const info = msg => {
+const info = (msg: string): void => {
   logToMain('info', msg)
 }
 
-const warn = msg => {
+const warn = (msg: string): void => {
   logToMain('warn', msg)
 }
 
-const error = msg => {
+const error = (msg: string): void => {
   logToMain('error', msg)
 }
 
