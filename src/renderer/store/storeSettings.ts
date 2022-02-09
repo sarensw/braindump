b25cbdd455cc37d6bae1ed8a3dcbee8b4a53ea34
sarensw <stephan.arenswald@daimler.com>
2022-02-09 04:27:05 +0100

    fix: show tabs default is true

--- src/renderer/store/storeSettings.ts
+++ src/renderer/store/storeSettings.ts
@@ -22,7 +22,7 @@ const initialState: Settings = {
   'editor.minimap.show': getDefaultValue<boolean>('editor.minimap.show') ?? false,
   'editor.linenumbers.show': getDefaultValue<boolean>('editor.linenumber.show') ?? true,
   'editor.wordwrap': getDefaultValue<boolean>('editor.wordwrap') ?? true,
-  'tabs.show': getDefaultValue<boolean>('tabs.show') ?? true,
+  'tabs.show': false,
   'presentation.title.subTitle': getDefaultValue<string>('presentation.title.subTitle') ?? ''
 }
 
