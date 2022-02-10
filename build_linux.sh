7f38600f881eccd393f3d4162f57bf14eb1c3679
Stephan Arenswald <stephan@sarensw.com>
2022-02-09 21:18:59 -0800

    chore: enable linux build

---
+++ build_linux.sh
@@ -0,0 +1,7 @@
+# set the node env
+export NODE_ENV=production
+
+# build
+npx esbuild src/main/index.js src/main/preload.js --outdir=build/main --bundle --platform=node --external:electron --format=cjs
+npx snowpack build
+npx electron-builder build --linux --publish never
