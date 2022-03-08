94674faaf6808e399f50018e47b75a94d0daa511
Stephan Arenswald <stephan@sarensw.com>
2022-03-08 04:42:08 +0100

    chore: remove settings from shared type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -10,18 +10,6 @@ export interface Dump extends File {
   title: string
 }
 
-export interface Settings {
-  'app.theme': string
-  'app.hotkeys.show': boolean
-  'backup.enabled': boolean
-  'backup.path': string
-  'editor.minimap.show': boolean
-  'editor.linenumbers.show': boolean
-  'editor.wordwrap': boolean
-  'tabs.show': boolean
-  'presentation.title.subTitle': string
-}
-
 export interface Snippet {
   name: string
   description: string
